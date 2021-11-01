d6d05fbdd6e9fcf76505b23adcd24dd243202f6a
Mohamed Elkazzaz <mohamedelkazzaz@qanvustech.com>
2021-11-01 15:51:30 +0200

    Initial Commit

---
+++ BMI-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI-Calculator
+//
+//  Created by Mohamed Elkazzaz on 01/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
